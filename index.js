47c456ea58bf2c97c0e25aac75dcd9ce00b4d0e4
Amimaro <amir.zahlan@gmail.com>
2018-04-19 08:17:52 -0300

    Generate template :sparkles:

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 'use strict'
 
-module.exports = function txtTemplate (options) {
-  options = options || {}
+const ejs = require('ejs')
+const fs = require('fs')
 
-  return true
+module.exports = async function txtTemplate(params) {
+  try {
+    let template = await readTXT(params.template)
+    let result = await render(unescapeQuotes(template), params.data)
+    await writeTXT(params.output, result)
+    return true
+  } catch (err) {
+    console.error(`An error occured: ${err}`)
+    return false
+  }
+}
+
+let readTXT = async function(path) {
+  return fs.readFileSync(path).toString('utf8')
+}
+
+let writeTXT = async function(path, data) {
+  return fs.writeFileSync(path, data)
+}
+
+let render = async function(template, data) {
+  return ejs.render(template, data)
+}
+
+let unescapeQuotes =  function(data) {
+  return data.replace(/(\'|\")+\</g, '<').replace(/\>(\'|\")+/g, '>').replace(/(\'\')/g, '\'').replace(/(\"\")/g, '\"')
 }
